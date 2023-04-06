66bfb55d2db6e1a9578a276368d585e3ce803368
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-04-06 17:29:36 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+    
+    for(i=0; i<n; i++) {
+        printf("iNeuron\n");
+    }
+    
+    return 0;
+}
